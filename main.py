f7fce7e5c7d57f6010eb4f6b1e536cf8170d9f3b
Anton898 <81709784+IvanovAnto@users.noreply.github.com>
2024-10-23 18:19:16 +0300

    homeWorck7.a

--- main.py
+++ main.py
@@ -3,16 +3,19 @@ import string
 # homeWorck7.1
 
 def say_hi(name, age):
-    print (f"Hi. My name is {name} and I'm {age} years old")
+    return f"Hi. My name is {name} and I'm {age} years old"
 
 # homeWorck7.2
 
 def correct_sentence(text):
     a = len(text)-1
     text[0].title()
+    newText = text
+    if not text[0].isupper(): # Метод capitalize делает первую букву заглавной
+        newText = text.capitalize()
     if text[len(text)-1] == ".":
-        return print(text.capitalize()) # Метод capitalize делает первую букву заглавной
-    else: return print(text.capitalize() + ".")
+        return newText
+    else: return newText + "."
 
 
 # homeWorck7.3
@@ -22,31 +25,48 @@ def second_index(text, some_str):
     if text.find(some_str) != -1: # сначала нахожу первый индекс совпадений с some_str
         i = text.find(some_str) + 1
         if text.find(some_str, i +1) != -1: # к первый индекс переношу в переменную i начинаю поиск со следующего индекса
-            return print(text.find(some_str, i + 1))
+            return text.find(some_str, i + 1)
         else:
-            return print("None")
+            return None
     else:
-            return print("None")
+            return None
+
+# homeWorck7.4
+
+def common_elements():
+    listOfTreeFive = []
+    i = 0
+    while i < 101:
+        if i % 3 == 0 and i % 5 == 0:
+            listOfTreeFive.append(i)
+        i += 1
+    return listOfTreeFive
+
 
 
 print("results homeWorck7.1")
 
-say_hi("Alex", 32)
-say_hi("Frank", 68)
+assert say_hi("Alex", 32) == "Hi. My name is Alex and I'm 32 years old", 'Test1'
+assert say_hi("Frank", 68) == "Hi. My name is Frank and I'm 68 years old", 'Test2'
+print('ОК')
 
 print("results homeWorck7.2")
 
-correct_sentence("greetings, friends") #== "Greetings, friends.", 'Test1'
-correct_sentence("hello")              # == "Hello.", 'Test2'
-correct_sentence("Greetings. Friends") #== "Greetings. Friends.", 'Test3'
-correct_sentence("Greetings, friends.") #== "Greetings, friends.", 'Test4'
-correct_sentence("greetings, friends.") #== "Greetings, friends.", 'Test5'
+assert correct_sentence("greetings, friends") == "Greetings, friends.", 'Test1'
+assert correct_sentence("hello") == "Hello.", 'Test2'
+assert correct_sentence("Greetings. Friends") == "Greetings. Friends.", 'Test3'
+assert correct_sentence("Greetings, friends.") == "Greetings, friends.", 'Test4'
+assert correct_sentence("greetings, friends.") == "Greetings, friends.", 'Test5'
+print('ОК')
 
 print("results homeWorck7.3")
 
-second_index("sims", "s") #== 3, 'Test1'
-second_index("find the river", "e") #== 12, 'Test2'
-second_index("hi", "h")# is None, 'Test3'
-second_index("Hello, hello", "lo")# == 10, 'Test4'
+assert second_index("sims", "s") == 3, 'Test1'
+assert second_index("find the river", "e") == 12, 'Test2'
+assert second_index("hi", "h") is None, 'Test3'
+assert second_index("Hello, hello", "lo") == 10, 'Test4'
+print('ОК')
 
 print("results homeWorck7.4")
+
+assert common_elements() == [0, 15, 30, 45, 60, 75, 90] # все работает только циры вывелись в другом порядке
